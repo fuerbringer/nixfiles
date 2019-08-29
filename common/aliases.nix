378ffe3cf5a5e1828546879184e6828adcecf17f
fuerbsev <fuerbsev@students.zhaw.ch>
2019-08-29 22:46:38 +0200

    Adjust aliases.

--- common/aliases.nix
+++ common/aliases.nix
@@ -1,29 +1,24 @@
 {
   environment.shellAliases = {
-    # Nix
-    nix-srch = "nix-env -Pqa | grep";
-
     # Editors
     v = "vim";
     sv = "sudoedit";
-    e = "emacs";
-    et = "emacs -nw"; # Terminal Emacs
+    e = "emacsclient";
+    et = "emacsclient -nw";
 
     # Git shortcuts
     g = "git";
     ga = "git add";
-    gb = "git branch";
     gc = "git commit -S -m";
-    gch = "git checkout";
-    gchmp = "git checkout master && git pull";
     gd = "git diff";
     gl = "git log";
     gpush = "git push";
     gpull = "git pull";
-    gr = "git remote -v";
     gs = "git status";
 
     # Misc
     mupdf = "mupdf-x11";
+    zath = "zathura";
+    neom = "neomutt"
   };
 }
